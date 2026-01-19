578b921e6a29a0cb554ccc63689d0d473bd7705e
neitan-02 <hernandezmorenonathan09@gmail.com>
2026-01-19 02:21:25 -0600

    feat: actualizacion de stilos para inicio de sesion

--- config/config.php
+++ config/config.php
@@ -1,17 +1,31 @@
 <?php 
 
-define ("CLIENT_ID", "Client ID");
+//Configuracion del sistema
+define("SITE_URL", "http://localhost/Tienda_online/");
+define ("KEY_TOKEN", "KEY_TOKEN");
+define ("MONEDA", "$");
+
+//Configuracion de Pay Pal
+define ("CLIENT_ID", "CLIENT_ID");
+define ("CURRENCY", "MXN");
+
+//Configuracion de Mercado
 define ("TOKEN_MP", "ACCES_TOKEN");
 define("PUBLIC_KEY_MP", "YOUR_PUBLIC_KEY");
-define ("CURRENCY", "MXN");
-define ("KEY_TOKEN", "APR.wqc-354*");
-define ("MONEDA", "$");
+define("LOCALE_MP", "es-MX");
+
+//Datos para envio de correo electronico 
+define("MAIL_HOST", "MAIL_HOST");
+define("MAIL_USER", "correo electronico");
+define("MAIL_PASS", "contraseña de gogle en caso de usar");
+define("MAIL_PORT", "465");
+
 
 session_start();
 
 $num_cart = 0;
-if(isset( $_SESSION['carrito']['productos'])) {
-    $num_cart = count( $_SESSION['carrito']['productos']);
+if(isset($_SESSION['carrito']['productos'])) {
+    $num_cart = count($_SESSION['carrito']['productos']);
 }
 
 ?>
